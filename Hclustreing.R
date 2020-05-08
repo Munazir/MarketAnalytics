d5e41d15f8799fc56b3888a60544ebb0a2348e3d
“mohussai” <mohussai@akamai.com>
2020-05-09 02:30:34 +0530

    h-clustering code

--- Hclustreing.R
+++ Hclustreing.R
@@ -2,7 +2,20 @@
 segment.data <- read.csv(file = "SegmentationData.csv",row.names = 1)
 head(segment.data)
 
+# data preprpcessing
 str(segment.data)
 dim(segment.data)
 
+#scaling
+scaled.data <- scale(segment.data[,c("Trendy","Styling","Reliability","Sportiness","Performance","Comfort")])
+head(scaled.data)
 
+#forming dist matrix
+dist.data <- dist(scaled.data,method = "euclidean")
+
+#check 5 rows and 5 columns
+as.matrix(dist.data)[1:5,1:5]
+
+#h-clustering
+data.clusters <- hclust(dist.data,method = "ward.D2")
+plot(data.clusters)
